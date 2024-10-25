b549e868408706d66eefca5bc60f326650227da2
evbanova <147727022+evbanova@users.noreply.github.com>
2024-10-25 19:58:17 +0300

    Add files via upload
    Homework1 - 22.10.24

---
+++ Walking median.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main() {
+    unsigned N = 0;
+    std::cin >> N;
+
+    int* A = new int[N];
+    float* arr_results = new float[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        std::cin >> num;
+        int j = i - 1;
+
+        while (j >= 0 && A[j] > num)
+        {
+            A[j + 1] = A[j];
+            j--;
+        }
+        A[j + 1] = num;
+
+        if ((i + 1) % 2)
+            arr_results[i] = A[i / 2];
+        else
+            arr_results[i] = (float)((A[i / 2] + A[i / 2 + 1])) / 2;
+    }
+
+    for (int i = 0; i < N; i++)
+        std::cout << arr_results[i] << std::endl;
+
+    delete[] A;
+    delete[] arr_results;
+
+}
